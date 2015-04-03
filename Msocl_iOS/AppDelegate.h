75cdaa8f3b7c6a65890ea479a523bef63c9bd3b9
imaginationcoder <labs@maisasolutions.com>
2015-04-03 15:41:43 +0530

    first commit

---
+++ Msocl_iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Msocl_iOS
+//
+//  Created by Maisa Solutions on 4/3/15.
+//  Copyright (c) 2015 Maisa Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
