f8bf76280dcba9452d37950957c8b150f6f82cc1
imaginationcoder <labs@maisasolutions.com>
2015-12-03 19:44:14 +0530

    Fix for UI after sharing to Whats app

--- Msocl_iOS/Controllers/Streams/PostDetailDescriptionViewController.m
+++ Msocl_iOS/Controllers/Streams/PostDetailDescriptionViewController.m
@@ -1685,6 +1685,26 @@
     {
         [textView1 setInputAccessoryView:inputView];
         [placeholderLabel removeFromSuperview];
+        
+        [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+        [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardDidShowNotification object:nil];
+        [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+        
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidShow:) name:UIKeyboardDidShowNotification object:nil];
+        
+        
+        DebugLog(@"postID:%@",postID);
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(keyboardWillShow:)
+                                                     name:UIKeyboardWillShowNotification
+                                                   object:nil];
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(keyboardWillHide:)
+                                                     name:UIKeyboardWillHideNotification
+                                                   object:nil];
+
         return YES;
 
     }
